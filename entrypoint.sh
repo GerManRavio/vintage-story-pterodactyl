32f239492d4ce7ce033cbc2ce8421ce347c604ce
GerManRavio <elias.trzoska@icloud.com>
2025-01-27 10:27:40 +0100

    removed INTERNAL_IP env

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@
 TZ=${TZ:-UTC}
 export TZ
 
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
