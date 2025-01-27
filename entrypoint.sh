41dfb11216c215de4228298f197d2894b07452a6
GerManRavio <elias.trzoska@icloud.com>
2025-01-27 10:17:28 +0100

    removed gameinstall

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,6 @@ printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 
 cd /home/container
 
-DOWNLOAD_URL="https://cdn.vintagestory.at/gamefiles/${RELEASE_TYPE}/vs_server_linux-x64_${VERSION}.tar.gz"
-curl -sSL -o vs_server.tar.gz $DOWNLOAD_URL
-tar -xzf vs_server.tar.gz
-rm vs_server.tar.gz
-
 dotnet --info
 
 exec env ${PARSED}
